1aa5bb129d11fc126831885968c5c06c8627f0a7
lind-08 <lind8584@gmail.com>
2016-03-11 22:55:27 +0300

    second commit

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+struct
 {
-    printf("Hello World!\n");
+    char *word;
+    int findsCount;
+};
+
+
+void checkFiles(int argc, char** argv)
+{
+    int i;
+    for (i = 1; i < argc; i++)
+    {
+        FILE *fp;
+        fp = fopen(argv[i],"rt");
+        if (fp == 0)
+        {
+            printf("Can't open file ");
+            puts(argv[i]);
+            break;
+        }
+        fclose(fp);
+    }
+}
+
+
+int main(int argc, char** argv)
+{
+    if (argc == 1)
+    {
+        printf("No input files.\n");
+    }
+    else
+    {
+        checkFiles(argc, argv);
+    }
     return 0;
 }
 
